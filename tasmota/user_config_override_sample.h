135e87f0d07250b9aaebe772afa84a9d244b1645
RJ-Bln-2026 <rjberlin@arcor.de>
2026-06-10 18:02:02 +0200

    Update user_config_override_sample.h

--- tasmota/user_config_override_sample.h
+++ tasmota/user_config_override_sample.h
@@ -1,94 +1,46 @@
-/*
-  user_config_override.h - user configuration overrides my_user_config.h for Tasmota
-
-  Copyright (C) 2021  Theo Arends
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// 002 config_override kleiner 1,5MB
 
 #ifndef _USER_CONFIG_OVERRIDE_H_
 #define _USER_CONFIG_OVERRIDE_H_
 
-/*****************************************************************************************************\
- * USAGE:
- *   To modify the stock configuration without changing the my_user_config.h file:
- *   (1) copy this file to "user_config_override.h" (It will be ignored by Git)
- *   (2) define your own settings below
- *
- ******************************************************************************************************
- * ATTENTION:
- *   - Changes to SECTION1 PARAMETER defines will only override flash settings if you change define CFG_HOLDER.
- *   - Expect compiler warnings when no ifdef/undef/endif sequence is used.
- *   - You still need to update my_user_config.h for major define USE_MQTT_TLS.
- *   - All parameters can be persistent changed online using commands via MQTT, WebConsole or Serial.
-\*****************************************************************************************************/
+// --- Unnötige, große Module deaktivieren ---
+#undef USE_MQTT
+#undef USE_BLE
+#undef USE_DOMOTICZ
+#undef USE_HOME_ASSISTANT
+#undef USE_KNX
+#undef USE_DISCOVERY
+#undef USE_I2C
+#undef USE_SPI
+#undef USE_DISPLAY
+#undef USE_IR_REMOTE
+#undef USE_EMULATION
+#undef USE_WEBCAM
+#undef USE_RC_SWITCH
+#undef USE_TUYA_MCU
+#undef USE_ZIGBEE
 
-/*
-Examples :
+// --- Energie für NOUS A8T ---
+#define USE_ENERGY_SENSOR
+#define USE_ENERGY_MARGIN_DETECTION
 
-// -- Master parameter control --------------------
-#undef  CFG_HOLDER
-#define CFG_HOLDER        4617                   // [Reset 1] Change this value to load SECTION1 configuration parameters to flash
+// --- Berry + Logik für dein Watchdog-Script ---
+#define USE_BERRY        // autoexec.be, webclient(), Treiber-Hooks, Timer
+#define USE_PING         // Ping-Auswertung
+#define USE_RULES        // Button-/Ping-Events
+#define USE_TIMERS       // Tasmota-Timer
+#define USE_CRON         // optionale Zeitsteuerung, klein
 
-// -- Setup your own Wifi settings  ---------------
-#undef  STA_SSID1
-#define STA_SSID1         "YourSSID"             // [Ssid1] Wifi SSID
+// --- Telegram via HTTPS (kleines TLS-Modul) ---
+#define USE_WEBSEND_TLS  // TLS für Berry-webclient (Telegram-API)
 
-#undef  STA_PASS1
-#define STA_PASS1         "YourWifiPassword"     // [Password1] Wifi password
+// --- Webserver (kompakt, aber vorhanden) ---
+#define USE_WEBSERVER
 
-// -- Setup your own MQTT settings  ---------------
-#undef  MQTT_HOST
-#define MQTT_HOST         "your-mqtt-server.com" // [MqttHost]
+// --- Zeit / Zeitzone ---
+#define USE_NTP
+#define USE_TIME_ZONE_DATA
+#define NTP_SERVER1 "0.de.pool.ntp.org"
+#define NTP_SERVER2 "1.de.pool.ntp.org"
 
-#undef  MQTT_PORT
-#define MQTT_PORT         1883                   // [MqttPort] MQTT port (10123 on CloudMQTT)
-
-#undef  MQTT_USER
-#define MQTT_USER         "YourMqttUser"         // [MqttUser] Optional user
-
-#undef  MQTT_PASS
-#define MQTT_PASS         "YourMqttPass"         // [MqttPassword] Optional password
-
-// You might even pass some parameters from the command line ----------------------------
-// Ie:  export PLATFORMIO_BUILD_FLAGS='-DUSE_CONFIG_OVERRIDE -DMY_IP="192.168.1.99" -DMY_GW="192.168.1.1" -DMY_DNS="192.168.1.1"'
-
-#ifdef MY_IP
-#undef  WIFI_IP_ADDRESS
-#define WIFI_IP_ADDRESS     MY_IP                // Set to 0.0.0.0 for using DHCP or enter a static IP address
 #endif
-
-#ifdef MY_GW
-#undef  WIFI_GATEWAY
-#define WIFI_GATEWAY        MY_GW                // if not using DHCP set Gateway IP address
-#endif
-
-#ifdef MY_DNS
-#undef  WIFI_DNS
-#define WIFI_DNS            MY_DNS               // If not using DHCP set DNS IP address (might be equal to WIFI_GATEWAY)
-#endif
-
-#ifdef MY_DNS2
-#undef  WIFI_DNS2
-#define WIFI_DNS2           MY_DNS2              // If not using DHCP set DNS IP address (might be equal to WIFI_GATEWAY)
-#endif
-
-// !!! Remember that your changes GOES AT THE BOTTOM OF THIS FILE right before the last #endif !!!
-*/
-
-
-
-
-
-#endif  // _USER_CONFIG_OVERRIDE_H_
